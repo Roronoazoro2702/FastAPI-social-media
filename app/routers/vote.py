a85f05f81c01c6eebe7d35cf7c70d9fd7115526a
Roronoazoro2702 <rajharshit2702@gmail.com>
2024-07-12 13:37:54 +0530

    sec update

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -23,8 +23,8 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
     found_vote = vote_query.first()
     if (vote.dir == 1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user{
-                                current_user.id} has already voted on post {vote.post_id}")
+            current_user_id = current_user.id
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user{current_user_id} has already voted on post {vote.post_id}")
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         db.commit()
